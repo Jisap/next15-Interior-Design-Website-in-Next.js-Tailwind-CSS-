4efde4824d5866410b7bd4c5dac5e65a744d6c5b
Jorge Matos <jisapjisap@gmail.com>
2025-09-13 15:57:46 +0200

    Add GSAP animations to About Page component for enhanced visual engagement. Integrate scroll-triggered effects for various sections, including title and mission, and update layout with new animation classes for improved user experience. Also, add @gsap/react dependency for animation support.

--- app/About/Page.jsx
+++ app/About/Page.jsx
@@ -1,11 +1,97 @@
+'use client'
+
 import Image from "next/image"
-import { processSteps } from "../constants";
-import { galleryItems } from "../constants";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import DesingProcess from "../_components/DesingProcess";
+import Galllery from "../_components/Galllery";
+
+gsap.registerPlugin(ScrollTrigger);
 
 
 const Page = () => {
+  const container = useRef();
+
+  useGSAP(() => {
+    // Animación del título principal del video
+    gsap.from(".title-animation", {
+        y: 50,
+        opacity: 0,
+        duration: 1,
+        ease: "power3.out",
+        delay: 0.2
+    });
+
+    // Animación de la sección "About Us"
+    gsap.from(".about-title-animation", {
+        scrollTrigger: {
+            trigger: ".about-title-container",
+            start: "top 80%",
+        toggleActions: "restart none none reset"
+        },
+        y: 50,
+        opacity: 0,
+        duration: 0.8,
+        stagger: 0.2
+    });
+
+    // Animación de la sección de visión
+    gsap.from(".vision-image-animation", {
+        scrollTrigger: {
+            trigger: ".vision-image-animation",
+            start: "top 80%",
+        toggleActions: "restart none none reset"
+        },
+        x: -100,
+        opacity: 0,
+        duration: 1,
+        ease: "power3.out"
+    });
+    gsap.from(".vision-text-animation p", {
+        scrollTrigger: {
+            trigger: ".vision-text-animation",
+            start: "top 80%",
+        toggleActions: "restart none none reset"
+        },
+        x: 100,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.3,
+        ease: "power3.out"
+    });
+
+    // Animación de la sección de misión
+    gsap.from(".mission-text-animation p", {
+        scrollTrigger: {
+            trigger: ".mission-text-animation",
+            start: "top 80%",
+        toggleActions: "restart none none reset"
+        },
+        x: -100,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.3,
+        ease: "power3.out"
+    });
+    gsap.from(".mission-image-animation", {
+        scrollTrigger: {
+            trigger: ".mission-image-animation",
+            start: "top 80%",
+        toggleActions: "restart none none reset"
+        },
+        x: 100,
+        opacity: 0,
+        duration: 1,
+        ease: "power3.out"
+    });
+
+
+  }, { scope: container });
+
   return (
-    <>
+    <div ref={container} className="overflow-hidden">
       {/* About title */}
       <div className="relative h-[60vh] mt-24 overflow-hidden flex items-center justify-center">
         <video
@@ -19,7 +105,7 @@ const Page = () => {
         </video>
 
         <div className="absolute top-0 left-0 w-full h-full bg-black/70 z-10 flex items-center justify-center">
-          <h1 className="text-white text-center text-[3rem] md:text-[5rem] font-bricolage z-20 px-4">
+          <h1 className="text-white text-center text-[3rem] md:text-[5rem] font-bricolage z-20 px-4 title-animation">
             About Us
           </h1>
         </div>
@@ -28,17 +114,17 @@ const Page = () => {
       {/* About Us */}
       <div className="bg-white py-10">
         {/* Top section */}
-        <div className="container mx-auto mb-10 px-4">
+        <div className="container mx-auto mb-10 px-4 about-title-container">
           <div className="flex flex-col lg:flex-row justify-between items-center">
-            <div className="lg:w-2/3 mb-6 lg:mb-0">
+            <div className="lg:w-2/3 mb-6 lg:mb-0 about-title-animation">
               <h1 className="text-3xl lg:text-8xl font-bold text-gray-800">
                 Interior & Exterior Solutions
               </h1>
             </div>
 
-            <div className="lg:w-1/3">
+            <div className="lg:w-1/3 about-title-animation">
               <div>
-                <h3 className="text-xl font-semibold text-gray-700">
+                <h3 className="text-xl font-semibold text-gray-700 ">
                   About Us
                 </h3>
 
@@ -53,7 +139,7 @@ const Page = () => {
         {/* vision section*/}
         <div className="container mx-auto px-4 mb-12">
           <div className="flex flex-col lg:flex-row items-center">
-            <div className="lg:w-1/2 mb-6 lg:mb-0 overflow-hidden rounded-lg">
+            <div className="lg:w-1/2 mb-6 lg:mb-0 overflow-hidden rounded-lg vision-image-animation">
               <Image
                 src="/ab-vision-image1.jpeg"
                 alt="vision"
@@ -63,7 +149,7 @@ const Page = () => {
               />
             </div>
 
-            <div className="lg:w-1/2 lg:pl-12">
+            <div className="lg:w-1/2 lg:pl-12 vision-text-animation">
               <p className="mb-4 text-gray-700 leading-relaxed">
                 <span className="text-gray-800 font-bold">Our </span>
                 vision is to become a leader in transforming spaces
@@ -91,7 +177,7 @@ const Page = () => {
         <div className="container mx-auto px-4 mb-12">
           <div className="flex flex-col lg:flex-row items-center">
            
-            <div className="lg:w-1/2 lg:pl-12">
+            <div className="lg:w-1/2 lg:pl-12 mission-text-animation order-2 lg:order-1">
               <p className="mb-4 text-gray-700 leading-relaxed">
                 <span className="text-gray-800">
                   At <strong>LivingLines</strong>, our mission is to craft inspiring, functional,
@@ -111,7 +197,7 @@ const Page = () => {
               </p>
             </div>
 
-            <div className="lg:w-1/2 mb-6 lg:mb-0 overflow-hidden rounded-lg">
+            <div className="lg:w-1/2 mb-6 lg:mb-0 overflow-hidden rounded-lg mission-image-animation order-1 lg:order-2">
               <Image
                 src="/ab-vision-image1.jpeg"
                 alt="vision"
@@ -127,93 +213,11 @@ const Page = () => {
       </div>
 
       {/* Design Process */}
-      <div className="bg-gray-100 px-[8%] lg:px-[12%] py-16">
-        <div className="container mx-auto px-4">
-          <div className="flex flex-col lg:flex-row justify-between items-center mb-12">
-            <div className="lg:w-1/2 mb-10 lg:mb-0">
-              <h1 className="text-[3.5rem] leading-[3.5rem] sm:text-6xl sm:leading-none lg:text-8xl lg:leading-none font-bricolage font-semibold">
-                Our Design Process
-              </h1>
-            </div>
-
-            <div className="lg:w-1/3 ">
-              <h3 className="uppercase tracking-wide font-semibold border-b border-black inline-block">
-                Process
-              </h3>
-              <p className="text-lg max-w-md">
-                Discover how our thoughtful process transforms ideas into personalized, functional, and beatifully styled spaces.
-              </p>
-            </div>
-          </div>
-
-          <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 pt-10">
-            {processSteps.map(({ id, imgSrc, title, description }) => (
-              <div key={id} className="w-full relative mb-10">
-                <div className="flex flex-col items-center cursor-pointer relative group">
-                  {/* Circle with image and number */}
-                  <div className="w-[170px] h-[170px] rounded-full shadow-lg flex items-center justify-center relative hover:-translate-y-1.5 transition-transform duration-300">
-                    <img
-                      src={imgSrc}
-                      alt="process-icon"
-                      className="w-[70px] h-[70px] transition-transform duration-500 ease-out items-center group-hover:-rotate-y-360"
-                    />
-                    <span className="absolute -top-3 right-10 bg-black text-white w-8 h-8 flex items-center justify-center rounded-full text-sm font-semibold select-none">
-                      {id}.
-                    </span>
-                  </div>
-
-                  {/* Title */}
-                  <h2 className="font-bricolage text-xl mt-6 mb-4 relative after:absolute after:botton-0 after:left-0 after:h-0.5 after:bg-black after:w-0 group-hover:after:w-full after:transition-width after:duration-300">
-                    {title}
-                  </h2>
-
-                  {/* Description */}
-                  <p className="text-center text-base max-w-[70%] mx-auto">
-                    {description}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+      <DesingProcess />
 
       {/* Gallery */}
-      <section className="px-[8%] lg:px-[12%] py-16">
-        <div className="container mx-auto px-4">
-          <h1 className="text-[3.5rem] leading-[3.5rem] lg:text-8xl lg:leading-none font-bricolage font-bold mb-10">
-            Gallery
-          </h1>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {galleryItems.map((item, idx) => (
-              <a
-                key={idx}
-                href={item.img}
-                data-lightbox={item.lightbox}
-                data-title={item.title}
-                className="group relative block overflow-hidden rounded-md"
-              >
-                <div className="relative w-full h-[400px]">
-                  <Image
-                    src={item.img}
-                    alt={item.title}
-                    fill
-                    className="object-cover transition-transform duration-500 group-hover:scale-105"
-                  />
-                </div>
-
-                <div className="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black to-transparent z-10 opacity-0 translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-500 ease-in-out">
-                  <h4 className="text-white text-3xl font-bricolage">
-                    {item.title}
-                  </h4>
-                </div>
-              </a>
-            ))}
-          </div>
-        </div>
-      </section>
-    </>
+      <Galllery />
+    </div>
   )
 }
 
